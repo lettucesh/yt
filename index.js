5461d6d23d9f77df2d7cb9912b8c7101f0f1ad66
lettucesh <151165906+lettucesh@users.noreply.github.com>
2024-02-09 02:45:04 -0300

    playlist feature

--- index.js
+++ index.js
@@ -1,31 +1,104 @@
-const fs = require('fs');
 const prompt = require("prompt-sync")({ sigint: true });
 const ytdl = require('ytdl-core');
+const fs = require('fs');
+const axios = require('axios');
+const dotenv = require('dotenv');
 
-const ytURL = prompt('insira url do youtube: ');
+dotenv.config();
 
-const audioStream = ytdl(ytURL, { quality: 'highestaudio' });
+const { apiKey } = process.env;
 
-audioStream.on('error', (err) => console.error('erro ao baixar: ', err));
-audioStream.on('end', () => console.log('baixado!'));
+const ask = prompt('playlist ou musica? p/m ');
 
+const playlistURL = ask === 'p' ? prompt('insira url da playlist: ') : null;
+const ytURL = ask === 'm' ? prompt('insira url do youtube: ') : null;
+
+// para playlistURL
+const getAllPlaylistVideos = async (playlistId, apiKey) => {
+    let arrayVideos = [];
+
+    const getPlaylistVideos = async (pageToken = null) => {
+        let apiUrl = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=1&playlistId=${playlistId}&key=${apiKey}`;
+
+        if (pageToken) apiUrl += `&pageToken=${pageToken}`;
+
+        const resp = await axios.get(apiUrl);
+
+        arrayVideos.push(...resp.data.items);
+
+        if (resp.data.nextPageToken) await getPlaylistVideos(resp.data.nextPageToken);
+    }
+
+    await getPlaylistVideos();
+
+    return arrayVideos.map(item => {
+        return {
+            title: item.snippet.title,
+            videoId: item.snippet.resourceId.videoId
+        }
+    })
+}
+// para ytURL
 const getYtTitle = async () => {
-    try {
-        const info = await ytdl.getBasicInfo(ytURL);
-        return info.videoDetails.title;
-        
-    } catch (error) {
-        console.error('erro ao obter informacoes do video:', error);
-        return null;
+    const info = await ytdl.getBasicInfo(ytURL);
+    return {
+        title: info.videoDetails.title,
+        videoId: info.videoDetails.videoId
     }
 }
 
-(async () => {
-    const ytTitle = await getYtTitle();
-
-    if (ytTitle) {
-        console.log('baixando:', ytTitle);
-        const fileStream = fs.createWriteStream(`C:/Users/sop/Documents/musicas/${ytTitle}.mp3`);
-        audioStream.pipe(fileStream);
+const fixFilename = (filename) => {
+    if (typeof filename !== 'string') {
+        return '';
     }
+    return filename.replace(/[\/\?<>\\:\*\|":]/g, ''); // remove caracteres inválidos
+};
+
+
+const downloadAudio = (video) => {
+    const videoTitle = fixFilename(video.title);
+    const filePath = `C:/Users/sop/Documents/musicas/${videoTitle}.mp3`;
+
+    if (fs.existsSync(filePath)) {
+        console.log(`${videoTitle} ja esta baixado`);
+        return;
+    }
+
+    const audioStream = ytdl(`https://www.youtube.com/watch?v=${video.videoId}`, { quality: 'highestaudio' });
+
+    console.log(`arquivos a ser baixado: ${video.title}`);
+    audioStream.on('error', (err) => console.error('erro ao baixar: ', err));
+    audioStream.on('end', () => console.log(`${video.title} baixado!`));
+
+    const fileStream = fs.createWriteStream(`C:/Users/sop/Documents/musicas/${videoTitle}.mp3`);
+    audioStream.pipe(fileStream);
+}
+
+(async () => {
+    if (playlistURL) {
+        const playlistIdMatch = playlistURL.match(/list=([\w-]+)/);
+        const playlistId = playlistIdMatch[1];
+
+        const videos = await getAllPlaylistVideos(playlistId, apiKey);
+        console.log('lista de videos da playlist:', videos);
+
+        // exportar o array pro pc
+        const videosJson = JSON.stringify(videos, null, 2)
+        try {
+            fs.writeFileSync(`./playlists/${Date.now()}.json`, videosJson);
+            console.log('array exportado com sucesso');
+        } catch (error) {
+            console.log('erro ao exportar o array: ', error);
+        }
+
+        videos.forEach((video, index) => {
+            downloadAudio(video);
+        })
+
+    } else if (ytURL) {
+        const ytTitle = await getYtTitle();
+        downloadAudio({ title: ytTitle.title, videoId: ytTitle.videoId });
+
+    } else console.log('url invalida');
+
 })();
